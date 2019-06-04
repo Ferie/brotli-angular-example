86d5e1136ece6e62f979fffaec2da4487f196339
Riccardo Andreatta <riccardo.andreatta@essenceglobal.com>
2019-06-04 18:56:44 +0100

    server file cleaned

--- server.js
+++ server.js
@@ -9,20 +9,18 @@ const serverPort = 6200;
 // Start the server
 const server = express();
 
-// // Serve the static files form the dist directory
-// server.use(express.static(__dirname + '/dist'));
-
+// Use plugin to serve Brotli files if browser supports them or fallback to Gzip
 server.use("/", expressStaticGzip(path.join(__dirname + '/dist'), {
     enableBrotli: true
 }));
 
 // Get the URL and redirect http to https in production environment else redirect to the main file
-// server.get(/.*/, function (req, res, next) {
-//     if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production')
-//         res.redirect('https://' + req.hostname + req.url);
-//     else
-//         res.sendFile(__dirname + '/dist/index.html');
-// });
+server.get(/.*/, function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production')
+        res.redirect('https://' + req.hostname + req.url);
+    else
+        res.sendFile(__dirname + '/dist/index.html');
+});
 
 // Start the server by listening on the default port or the one configurated before
 server.listen(process.env.PORT || serverPort, function () {
